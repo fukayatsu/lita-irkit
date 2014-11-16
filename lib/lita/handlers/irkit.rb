8125a068cbed2e6ffb895ba387bc1ae30a02a53b
fukayatsu <fukayatsu@gmail.com>
2014-11-16 17:09:18 +0900

    Add `ir all off`

--- lib/lita/handlers/irkit.rb
+++ lib/lita/handlers/irkit.rb
@@ -9,10 +9,11 @@ module Lita
         handler_config.clientkey = ENV['IRKIT_CLIENTKEY']
       end
 
-      route /^ir list/,            :ir_list,       command: false, help: { "ir list"                      => "list irkit command names" }
-      route /^ir send (.+)/,       :ir_send,       command: false, help: { "ir send [command_name]"       => "send irkit command" }
-      route /^ir register (.+)/,   :ir_register,   command: true,  help: { "ir register [command_name]"   => "register irkit command" }
-      route /^ir unregister (.+)/, :ir_unregister, command: true,  help: { "ir unregister [command_name]" => "unregister irkit command" }
+      route /^ir list/,            :ir_list,         command: false, help: { "ir list"                      => "list irkit command names" }
+      route /^ir send (.+)/,       :ir_send,         command: false, help: { "ir send [command_name]"       => "send irkit command" }
+      route /^ir all off/,         :ir_send_all_off, command: false, help: { "ir all off"                   => "send irkit commands which end with 'off'" }
+      route /^ir register (.+)/,   :ir_register,     command: true,  help: { "ir register [command_name]"   => "register irkit command" }
+      route /^ir unregister (.+)/, :ir_unregister,   command: true,  help: { "ir unregister [command_name]" => "unregister irkit command" }
 
       def ir_list(response)
         response.reply Lita.redis.keys('irkit:messages:*').map{|key| key.gsub(/^irkit:messages:/, '')}.join(', ')
@@ -23,23 +24,45 @@ module Lita
         ir_data = irkit_api.get('/1/messages', clientkey: config.clientkey).body
         return response.reply "ir data not found" if ir_data.length == 0
 
-        Lita.redis["irkit:messages:#{cmd}"] = JSON.parse(ir_data)['message'].to_json
+        Lita.redis[key_for_cmd(cmd)] = JSON.parse(ir_data)['message'].to_json
         response.reply ":ok_woman:"
       end
 
       def ir_send(response)
         cmd = response.matches[0][0]
-        message = Lita.redis["irkit:messages:#{cmd}"]
+        message = Lita.redis[key_for_cmd(cmd)]
         return response.reply 'ir data not found' unless message
 
-        irkit_api.post('/1/messages', clientkey: config.clientkey, deviceid: config.deviceid, message: message)
+        send_message(message)
         response.reply ":ok_woman:"
+      end
+
+      def ir_send_all_off(response)
+        keys = Lita.redis.keys(key_for_all_off_cmd)
+        keys.each do |key|
+          message = Lita.redis[key]
+          send_message(message)
+        end
       end
 
       def ir_unregister(response)
         cmd = response.matches[0][0]
-        Lita.redis.del "irkit:messages:#{cmd}"
+        Lita.redis.del key_for_cmd(cmd)
         response.reply ":ok_woman:"
+      end
+
+    private
+
+      def key_for_cmd(cmd)
+        "irkit:messages:#{cmd}"
+      end
+
+      def key_for_all_off_cmd
+        "irkit:messages:*off"
+      end
+
+      def send_message(message)
+        irkit_api.post('/1/messages', clientkey: config.clientkey, deviceid: config.deviceid, message: message)
       end
 
       def config
