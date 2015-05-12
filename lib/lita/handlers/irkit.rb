13166895595c6e6045bd62caa9907edbc855d0d2
fukayatsu <fukayatsu@gmail.com>
2015-05-12 17:38:53 +0900

    Tweak message of ir register command

--- lib/lita/handlers/irkit.rb
+++ lib/lita/handlers/irkit.rb
@@ -20,6 +20,7 @@ module Lita
 
       def ir_register(response)
         cmd     = response.matches[0][0]
+        response.reply "waiting for ir data..."
         ir_data = irkit_api.get('messages', clientkey: config.clientkey, clear: 1).body
         return response.reply "ir data not found" if ir_data.length == 0
 
